bbc6842ee920245c878d1268c280842fcdef70f0
Aditya Alok <dev.aditya.alok@gmail.com>
2025-07-05 13:22:40 +0530

    fix4: Test 2

--- files/scripts/install-cachyos-kernel.sh
+++ files/scripts/install-cachyos-kernel.sh
@@ -16,8 +16,8 @@ dnf5 -y install kernel-cachyos
 curl -Lo /etc/yum.repos.d/_copr-cachyos-kernel-addons.repo \
   https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos-addons/repo/fedora-"${OS_VERSION}"/bieszczaders-kernel-cachyos-addons-fedora-"${OS_VERSION}".repo
 
-dnf5 install libcap-ng-devel procps-ng-devel
-dnf5 install uksmd
+dnf5 -y install libcap-ng-devel procps-ng-devel
+dnf5 -y install uksmd
 
 dnf5 versionlock add kernel-cachyos libcap-ng-devel procps-ng-devel uksmd
 
